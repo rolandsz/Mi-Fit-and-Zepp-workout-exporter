3a79886b4fc6ab55aa4405e97109623788fb2a3c
korsakov.iyu <korsakov.iyu@dns-shop.ru>
2022-07-14 16:37:04 +0800

    project changed workout-exporter: - Changed track name to date for ease of navigation

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from datetime import datetime
 
 from src.api import Api
 from third_party.gpx_file_exporter import GpxFileExporter
@@ -29,25 +30,30 @@ class Scraper:
 
         for history in history["data"]["summary"]:
             track_id = history["trackid"]
-            logging.info(f"Downloading workout {track_id}")
 
-            os.makedirs(self.get_output_dir_path(track_id), exist_ok=True)
+            # timestamp -> str
+            track_date = int(track_id)
+            track_date = datetime.fromtimestamp(track_date).strftime('%Y-%m-%d')
+
+            logging.info(f"Downloading workout {track_date}")
+
+            os.makedirs(self.get_output_dir_path(track_date), exist_ok=True)
 
             # history.json
-            history_file_path = self.get_history_file_path(track_id)
+            history_file_path = self.get_history_file_path(track_date)
 
             with open(history_file_path, encoding="utf8", mode="w") as f:
                 json.dump(history, f)
 
             # detail.json
             detail = self.api.get_detail(track_id, history["source"])
-            detail_file_path = self.get_detail_file_path(track_id)
+            detail_file_path = self.get_detail_file_path(track_date)
 
             with open(detail_file_path, encoding="utf8", mode="w") as f:
                 json.dump(detail, f)
 
             # track_id.gpx
             exporter = GpxFileExporter(
-                self.get_gpx_file_path(track_id), history, detail["data"]
+                self.get_gpx_file_path(track_date), history, detail["data"]
             )
             exporter.export()
